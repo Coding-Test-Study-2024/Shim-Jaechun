8cf3e54eeed3282d226d0c08270f58b79dbcb63b
ShimJaeChun <shimnaldo98@gmail.com>
2024-04-18 11:35:43 +0900

    feat : 실버 문제1 프로그래머스 최댓값과 최솟값추가

---
+++ Week-7/Silver/PGM-MinMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+public class Solution
+{
+    public string solution(string s)
+    {
+        StringBuilder answer = new StringBuilder();
+
+        string[] temp = s.Split(' ');
+
+        int mn = int.MaxValue;
+        int mx = int.MinValue;
+
+        foreach (string str in temp)
+        {
+            mn = Math.Min(int.Parse(str), mn);
+            mx = Math.Max(int.Parse(str), mx);
+        }
+
+        answer.Append(mn.ToString());
+        answer.Append(" ");
+        answer.Append(mx.ToString());
+
+        return answer.ToString();
+    }
+}
