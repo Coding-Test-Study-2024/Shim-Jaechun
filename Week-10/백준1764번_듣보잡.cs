3fe2fe3ffd3da2bac6f70a23f7a7f8d406f8a743
심재천 <shimnaldo98@gmail.com>
2024-10-05 18:53:14 +0900

    feat : 백준 1764번 : 듣보잡 (해시)

---
+++ Week-10/백준1764번_듣보잡.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace 백준1764번_듣보잡
+{
+    class Program
+    {
+        static List<string> solution(string[] mainArr, string[] subArr)
+        {
+            List<string> answer = new List<string>();
+
+            // unordered_set
+            HashSet<string> hashSet = new HashSet<string>();
+
+            for (int i = 0; i < mainArr.Length; i++)
+                hashSet.Add(mainArr[i]);
+
+            for (int i = 0; i < subArr.Length; i++)
+                if (hashSet.Contains(subArr[i]))
+                    answer.Add(subArr[i]);
+
+            answer.Sort();
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 0, m = 0;
+            string[] nm = Console.ReadLine().Split(' ');
+            n = int.Parse(nm[0]);
+            m = int.Parse(nm[1]);
+
+            string[] nPeps = new string[n];
+            for (int i = 0; i < n; i++)
+                nPeps[i] = Console.ReadLine();
+
+            string[] mPeps = new string[m];
+            for (int i = 0; i < m; i++)
+                mPeps[i] = Console.ReadLine();
+
+            // 1. 요소가 더 많은 배열을 기준 배열로 해시에 할당
+            // 2. 요소가 적은 배열을 순회하며 해시에 요소가 존재하는지 확인
+            List<string> answers = n > m ? solution(nPeps, mPeps) : solution(mPeps, nPeps);
+
+            Console.WriteLine(answers.Count);
+            foreach (string name in answers)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
