d0692cab1d8af42d404a87db3a2c11b4221f9181
심재천 <shimnaldo98@gmail.com>
2024-10-22 18:50:34 +0900

    feat : 백준 11279번 - 최대 힙

---
+++ Week-12/백준11279번_최대 힙.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace 백준11279번_최대_힙
+{
+    class Program
+    {
+        static StringBuilder solution(int n, int[] arr)
+        {
+            StringBuilder answer = new StringBuilder();
+
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    if (pq.Count == 0)
+                        answer.AppendLine("0");
+                    else
+                        answer.AppendLine($"{pq.Dequeue()}");
+                }
+                else
+                    pq.Enqueue(arr[i], -arr[i]);
+            }
+
+            return answer;
+        }
+
+        static void Main(string[] argc)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(solution(n, arr).ToString());
+        }
+    }
+}
